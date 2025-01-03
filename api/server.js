29dbe3976892f1c08e18884a5b5a11cbdee7caec
Pujan-khunt <pujankhunt2412@gmail.com>
2025-01-03 23:17:59 +0530

    updated server.js

--- api/server.js
+++ api/server.js
@@ -1,19 +1,9 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const { Server } = require('socket.io')
-const server = http.createServer(app);
-const io = new Server(server);
 const PORT = 3000;
 
 app.use(express.static('static'));
 
-io.on('connection', (socket) => {
-    socket.on('draw', (data) => {
-        socket.broadcast.emit('draw', data);
-    })
-})
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Real Time Whiteboard Application Running At http://localhost:${PORT}`);
 })
