07e579245857aae59853a9e11ad607671302b029
Pujan-khunt <pujankhunt2412@gmail.com>
2025-01-04 14:25:21 +0530

    Made the whiteboard collaborative

--- api/server.js
+++ api/server.js
@@ -1,9 +1,35 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
+    const express = require('express');
+    const http = require('http');
+    const { Server } = require('socket.io');
 
-app.use(express.static('static'));
+    const app = express();
+    const server = http.createServer(app);
+    const io = new Server(server);
+    const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Real Time Whiteboard Application Running At http://localhost:${PORT}`);
-})
+    let users = 0;
+
+    app.use(express.static('static'));
+
+    io.on('connection', (socket) => {
+        users++;
+        io.emit('userCount', users);
+
+        socket.on('drawing', (data) => {
+            socket.broadcast.emit('drawing', data);
+        });
+
+        socket.on('clearBoard', () => {
+            io.emit('clearBoard');
+        })
+
+        socket.on('disconnect', () => {
+            users--;
+            io.emit('userCount', users);
+        });
+
+    });
+
+    server.listen(PORT, () => {
+        console.log(`Real Time Whiteboard Application Running At http://localhost:${PORT}`);
+    });
--- static/script.js
+++ static/script.js
@@ -1,56 +1,89 @@
+const socket = io();
+
+// canvas properties
 const canvas = document.querySelector('#whiteboard');
 const ctx = canvas.getContext('2d');
-console.log(canvas.getBoundingClientRect());
 canvas.width = 1000;  
 canvas.height = 600;
 
-const penSize = document.querySelector('#penSize');
-
+// Handling of clearing whiteboard
 const clearBtn = document.querySelector('#clearBoard');
 clearBtn.addEventListener('click', () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    socket.emit('clearBoard');
 });
 
-let painting = false;
-let lastX = 0;
-let lastY = 0;
+let drawingMode = false;
 
+// calculating offset of canvas based on its location
 function calculateCanvasOffset() {
     const data = canvas.getBoundingClientRect();
     return { x: data.left, y: data.top };
 }
 
+// drawing single points and also act as a starting point for a stroke
 canvas.addEventListener('mousedown', (event) => {
-    painting = true;
-
-    const offset = calculateCanvasOffset();
-    lastX = event.clientX - offset.x;
-    lastY = event.clientY - offset.y;
-});
-
-canvas.addEventListener('mouseup', () => {
-    painting = false;
+    drawingMode = true;
     
-    // reset the drawing mode
-    ctx.beginPath(); 
+    const offset = calculateCanvasOffset();
+    const X = event.clientX - offset.x;
+    const Y = event.clientY - offset.y;
+    
+    draw(X, Y, false);
+    socket.emit('drawing', {
+        x: X,
+        y: Y,
+        isStroke: false,
+        color: ctx.strokeStyle,
+        penSize: ctx.lineWidth
+    })
 });
+
+canvas.addEventListener('mouseout', () => drawingMode = false);
+canvas.addEventListener('mouseup', () => drawingMode = false);
 
 canvas.addEventListener('mousemove', (event) => {
-    // dont draw unless the mouse is down
-    if (!painting) return;
+    if(!drawingMode) return;
 
     const offset = calculateCanvasOffset();
-    const x = event.clientX - offset.x;
-    const y = event.clientY - offset.y;
+    const X = event.clientX - offset.x;
+    const Y = event.clientY - offset.y;
     
-    const thickness = penSize.value;
-
-    ctx.lineWidth = thickness;
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = document.getElementById('colorPicker').value;
-
-    ctx.lineTo(x, y);
-    ctx.stroke();
-    ctx.beginPath();  
-    ctx.moveTo(x, y); 
+    draw(X, Y, true);
+    socket.emit('drawing', {
+        x: X,
+        y: Y,
+        isStroke: true,
+        color: ctx.strokeStyle,
+        penSize: ctx.lineWidth
+    });
 });
+
+function draw(X, Y, isStroke) {
+    ctx.lineWidth = document.querySelector('#penSize').value || 2; /* Default size for pen */
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = document.getElementById('colorPicker').value || '#000000'; /* Default color (black) */
+
+    if(isStroke) {
+        ctx.lineTo(X, Y);
+        ctx.stroke();
+    } else{
+        ctx.beginPath();
+        ctx.moveTo(X, Y);
+    }
+}
+
+socket.on('drawing', (data) => {
+    ctx.strokeStyle = data.color;
+    ctx.lineWidth = data.penSize;
+    draw(data.x, data.y, data.isStroke);
+})
+
+socket.on('clearBoard', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+})
+
+socket.on('userCount', (users) => {
+    document.querySelector('#display-users').innerText = `Connected Users: ${users}`;
+})
+
